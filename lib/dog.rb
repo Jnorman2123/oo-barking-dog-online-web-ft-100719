5496d212c8d02b3aed02dd26652aea0db095a6b7
Jonathan Norman <jnorman2123@gmail.com>
2019-10-12 18:12:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,11 @@
 # Your code goes here!
 
-class Dog 
+class Dog
   def name=(dog_name)
     @this_dogs_name = dog_name
-  end 
+  end
 
-  def name 
+  def name
     @this_dogs_name
-  end 
-end 
-
-
-
+  end
+end
